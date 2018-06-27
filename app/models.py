a656e47cc7119447d2cba7e8bda4ec045960e61a
stux4a <lets.jam@gmail.com>
2018-06-27 15:24:04 -0700

    added followers; issue with db migrations

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id'))
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
                      )
 
 class User(UserMixin, db.Model):
@@ -19,8 +19,8 @@ class User(UserMixin, db.Model):
 
   followed = db.relationship(
       'User', secondary=followers,
-      primaryjoin()=(followers,c.follower_id == id),
-      secondaryjoin()=(followers,c.follower_id == id),
+      primaryjoin=(followers.c.follower_id == id),
+      secondaryjoin=(followers.c.followed_id == id),
       backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
   def __repr__(self):
@@ -31,6 +31,25 @@ class User(UserMixin, db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def follow(self, user):
+    if not self.is_following(user):
+      self.followed.append(user)
+
+  def unfollow(self, user):
+    if self.is_following(user):
+      self.followed.remove(user)
+
+  def is_following(self, user):
+    return self.followed.filter(
+        followers.c.follower_id == user.id),count() > 0
+    #TODO test return user.id in self.followed.c.follower_id
+
+  def followed_posts(self):
+    return Post.query.join(
+        followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.followed_id == self.id).order_by(
+                Post.timestamp.desc())
 
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
